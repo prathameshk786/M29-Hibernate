4481e45e180113c0d4353671db6fafee6ad85a6b
2144AX <2144AX@Prathamesh>
2023-06-28 16:39:18 +0530

    Commit

---
+++ daytwo/src/org/tnsif/singletableinheritance/Employee.java
@@ -0,0 +1,50 @@
+//program to demonstrate on Single-table inheritance
+package org.tnsif.singletableinheritance;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+@Entity
+@Table(name="Employee")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="Emp_type",discriminatorType=DiscriminatorType.STRING)
+@DiscriminatorValue("EMP")
+//parent class
+public class Employee {
+	
+	//private data members
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int empId;
+	private String empName;
+	private float salary;
+	
+	//GETTERS AND SETTERS
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	
+	
+
+}
